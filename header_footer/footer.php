3d63edb1be42c5c1eb53c40d5a30fa6afc2b45b1
Chia Hou Tan <tanchiahou5309@hotmail.com>
2020-03-12 13:54:06 +0000

    footer header php add and update

---
+++ header_footer/footer.php
@@ -0,0 +1,3 @@
+<footer>
+<p class="textBold">&copy; <?php echo date("Y"); ?> PlayStation 4 Games Website, by CHIA HOU TAN. (Server Side Development CA1)</p>
+</footer>
---
+++ header_footer/header.php
@@ -0,0 +1,3 @@
+<header>
+<img class="headerImg" src='logo\PSStore.png'></img><h1>PlayStation 4 Games Store</h1>
+</header>
