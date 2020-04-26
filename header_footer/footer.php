6ed991b17a4e46477502195aab728c0ecb499845
Chia Hou Tan <tanchiahou5309@hotmail.com>
2020-04-26 19:31:17 +0100

    footer update

--- header_footer/footer.php
+++ header_footer/footer.php
@@ -1,3 +1,3 @@
 <footer>
-<p class="textBold">&copy; <?php echo date("Y"); ?> PlayStation 4 Games Website, by CHIA HOU TAN. (Server Side Development CA1)</p>
+<p class="textBold">&copy; <?php echo date("Y"); ?> PlayStation 4 Games Website, by CHIA HOU TAN. (Server Side Development CA3)</p>
 </footer>
